d5ade28bbf4f591a5ada11d9a591c0516cde717d
luisvava <luismanuel.vadillo.vargas.alu@iesfernandoaguilar.es>
2023-05-11 01:16:22 +0200

    14 commit

--- boletin/views.py
+++ boletin/views.py
@@ -9,17 +9,28 @@ def inicio(request):
     if request.user.is_authenticated:
         titulo = "Bienvenido %s" %(request.user)
     form = RegModelForm(request.POST or None)
+
+    context = {
+        "titulo" : titulo,
+        "el_form" : form,
+    }
+    
+
     if form.is_valid():
         instance = form.save(commit=False)
+        nombre=form.cleaned_data.get("nombre")
+        email=form.cleaned_data.get("email")
+        if not instance.nombre:
+            instance.nombre = "PERSONA"
         #form_data = form.cleaned_data
         instance.save()
+        context = {
+            "titulo": "Gracias %s!" %(nombre)
+        }
         print(instance)
         print(instance.timestamp)
         #abc = (form_data.get("email"))
         #abc2 = (form_data.get("nombre"))
         #obj = Registrado.objects.create(email=abc, nombre=abc2)
-    context = {
-        "titulo" : titulo,
-        "el_form" : form,
-    }
+
     return render(request, "inicio.html", context)
