f2310906e2f7203768513b40a1b8548c8fad87ad
luisvava <luismanuel.vadillo.vargas.alu@iesfernandoaguilar.es>
2023-05-10 23:55:35 +0200

    11 commit

--- boletin/forms.py
+++ boletin/forms.py
@@ -7,6 +7,15 @@ class RegModelForm(forms.ModelForm):
         model=Registrado
         fields = ["nombre", "email"]
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_base, proveedor = email.split("@")
+        dominio,extension = proveedor.split(".")
+        if not extension == "edu":
+            raise forms.ValidationError("Correo no Valido")
+        return email
+
+        
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     email = forms.EmailField()
