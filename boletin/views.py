3cc641491e1c83e7ef6ebb063608ecbad02422b1
luisvava <luismanuel.vadillo.vargas.alu@iesfernandoaguilar.es>
2023-05-10 23:37:45 +0200

    7 commit

--- boletin/views.py
+++ boletin/views.py
@@ -4,6 +4,8 @@ from .forms import RegForm
 # Create your views here.
 def inicio(request):
     form = RegForm()
+    if form.is_valid():
+        print(form.cleaned_data)
     context = {
         "el_form" : form,
     }
